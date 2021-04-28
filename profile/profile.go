6879e06cf1d4e7459d4a7bbae2387460370d8173
thisisnew <thisisnew@naver.com>
2021-04-28 15:32:47 +0900

    api 생성 연습

---
+++ profile/profile.go
@@ -0,0 +1,116 @@
+package profile
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type Profiles struct {
+	gorm.Model
+	ID      string `gorm:"primary_key"`
+	Name    string
+	Age     string
+	Company string
+	IsAdmin string `gorm:"column:is_admin"`
+}
+
+const dsn string = "root:1234@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+
+func GetProfileList(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var profiles []Profiles
+
+	db.Find(&profiles)
+
+	if len(profiles) == 0 {
+		json.NewEncoder(w).Encode(map[string]string{"result": "데이터가 없습니다."})
+		return
+	}
+
+	json.NewEncoder(w).Encode(profiles)
+}
+
+func GetProfile(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	p := mux.Vars(r)
+	id := p["id"]
+
+	profiles := Profiles{}
+	db.First(&profiles, id)
+
+	if profiles.Name == "" {
+		json.NewEncoder(w).Encode(map[string]string{"result": "데이터가 없습니다."})
+		return
+	}
+
+	json.NewEncoder(w).Encode(profiles)
+}
+
+func insertProfile(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	newProfile := Profiles{}
+	err = json.NewDecoder(r.Body).Decode(&newProfile)
+
+	result := db.Create(&newProfile)
+
+	if result.RowsAffected > 0 {
+		json.NewEncoder(w).Encode(map[string]int{"status": http.StatusOK})
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]int{"status": http.StatusConflict})
+}
+
+func updateProfile(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	modifyProfile := Profiles{}
+	err = json.NewDecoder(r.Body).Decode(&modifyProfile)
+
+	var profiles Profiles
+	result := db.Model(&profiles).Where("id = ?", modifyProfile.ID).Updates(Profiles{Name: modifyProfile.Name, Age: modifyProfile.Age, Company: modifyProfile.Company, IsAdmin: modifyProfile.IsAdmin})
+
+	if result.RowsAffected == 0 {
+		json.NewEncoder(w).Encode(map[string]int{"status": http.StatusNotModified})
+	} else {
+		json.NewEncoder(w).Encode(map[string]int{"status": http.StatusCreated})
+	}
+}
+
+func deleteProfile(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	p := mux.Vars(r)
+	id := p["id"]
+
+	var profiles Profiles
+	db.Where("id = ?", id).Delete(&profiles)
+
+	json.NewEncoder(w).Encode(map[string]int{"status": http.StatusOK})
+}
--- vehicle/vehicle.go
+++ vehicle/vehicle.go
@@ -26,48 +26,126 @@ type PageInfo struct {
 }
 
 type Vehicle struct {
-	LicensePlateNumber string    `json:"licensePlateNumber"`
-	VehicleModel       string    `json:"vehicleModel"`
-	Vin                string    `json:"vin"`
-	ControllerID       string    `json:"controllerId"`
-	JudgementTime      time.Time `json:"judgementTime"`
-	UserID             string    `json:"userId"`
-	UserName           string    `json:"userName"`
-	PhoneNumber        string    `json:"phoneNumber"`
-	StartLatitude      int64     `json:"startLatitude"`
-	StartLongitude     int64     `json:"startLongitude"`
-	EndLatitude        int64     `json:"endLatitude"`
-	EndLongitude       int64     `json:"endLongitude"`
-	NotifyTime         time.Time `json:"notifyTime"`
-	NotifyTarget       string    `json:"notifyTarget"`
-	NotifyMsg          string    `json:"notifyMsg"`
-	Memo               string
-	CreatedBy          string    `json:"createdBy"`
-	CreatorName        string    `json:"creatorName"`
-	UpdatedBy          string    `json:"updatedBy"`
-	UpdatorName        string    `json:"updatorName"`
-	BusinessId         string    `json:"businessId"`
-	BranchId           int64     `json:"branchId"`
-	OperationTime      time.Time `json:"operationTime"`
-	VehicleId          string    `json:"vehicleId"`
+	Id                   string    `json:"ID"`
+	Name                 string    `json:"name"`
+	Vin                  string    `json:"VIN"`
+	LicensePlateNumber   string    `json:"licensePlateNumber"`
+	ControllerId         string    `json:"controllerId"`
+	RegistrationDate     string    `json:"registrationDate"`
+	ProductionDate       string    `json:"productionDate"`
+	GarageAddress        string    `json:"garageAddress"`
+	Color                string    `json:"color"`
+	Transmission         string    `json:"transmission"`
+	Business             Business  `json:"business" gorm:"-"`
+	Location             string    `json:"location"`
+	Model                Model     `json:"model" gorm:"-"`
+	Insurance            Insurance `json:"insurance" gorm:"-"`
+	Age                  uint      `json:"age"`
+	Available            bool      `json:"available"`
+	State                State     `json:"state" gorm:"-"`
+	Payments             string    `json:"payments"`
+	Branch               Branch    `json:"branch" gorm:"-"`
+	Order                uint      `json:"order"`
+	VehicleType          string    `json:"vehicleType"`
+	ModelYear            uint      `json:"modelYear"`
+	ReservationUsage     string    `json:"reservationUsage"`
+	ReservationStatus    string    `json:"reservationStatus"`
+	ReservationStartDate uint      `json:"reservationStartDate"`
+	ReservationEndDate   uint      `json:"reservationEndDate"`
+}
+
+type Branch struct {
+	Id                 uint   `json:"id"`
+	BusinessId         string `json:"businessId"`
+	Name               string `json:"name"`
+	BusinessRegNum     string `json:"businessRegNum"`
+	CompanyBusiness    string `json:"companyBusiness"`
+	BusinessType       string `json:"businessType"`
+	PhoneNumber        string `json:"phoneNumber"`
+	FaxNumber          string `json:"faxNumber"`
+	ManagerName        string `json:"managerName"`
+	ManagerPhoneNumber string `json:"managerPhoneNumber"`
+	ManagerEmail       string `json:"managerEmail"`
+	BankName           string `json:"bankName"`
+	AccountNumber      string `json:"accountNumber"`
+	AccountHolder      string `json:"accountHolder"`
+}
+
+type Business struct {
+	Id           string `json:"ID"`
+	Name         string `json:"name"`
+	BusinessType string `json:"businessType"`
+	PublicRide   bool   `json:"publicRide"`
+	PrivateRide  bool   `json:"privateRide"`
+	TestRide     bool   `json:"testRide"`
+	AutoConfirm  bool   `json:"autoConfirm"`
+	BusinessPath bool   `json:"businessPath"`
+	PersonalPath bool   `json:"personalPath"`
+	BusinessUse  bool   `json:"businessUse"`
+	PersonalUse  bool   `json:"personalUse"`
+}
+
+type Insurance struct {
+	Age      uint   `json:"age"`
+	Company  string `json:"company"`
+	Pd       string `json:"PD"`
+	Bi1      string `json:"BI1"`
+	Bi2      string `json:"BI2"`
+	Db       string `json:"DB"`
+	Uvd      string `json:"UVD"`
+	Ibi      string `json:"IBI"`
+	Ipd      string `json:"IPD"`
+	Idb      string `json:"IDB"`
+	Isd      string `json:"ISD"`
+	JoinDate string `json:"joinDate"`
+}
+
+type Model struct {
+	Id               string `json:"ID"`
+	Name             string `json:"name"`
+	Brand            string `json:"brand"`
+	Standard         string `json:"standard"`
+	StandardModelId  string `json:"standardModelId"`
+	SeatingCapacity  string `json:"seatingCapacity"`
+	FuelType         string `json:"fuelType"`
+	FuelEfficiency   string `json:"fuelEfficiency"`
+	FuelTankCapacity string `json:"fuelTankCapacity"`
+	Displacement     string `json:"displacement"`
+	Grade            string `json:"grade"`
+	WarmUpTime       string `json:"warmUpTime"`
+	ImageUrl         string `json:"imageUrl"`
+}
+
+type State struct {
+	Latitude  uint   `json:"latitude"`
+	Longitude uint   `json:"longitude"`
+	Odometer  string `json:"odometer"`
+	Dte       string `json:"DTE"`
+	FuelLevel string `json:"fuelLevel"`
+	Soc       uint   `json:"SOC"`
+	Charging  bool   `json:"charging"`
+	Battery   string `json:"battery"`
+	Speed     string `json:"speed"`
+	TpmsFl    string `json:"tpmsFl"`
+	TpmsFr    string `json:"tpmsFr"`
+	TpmsRl    string `json:"tpmsRl"`
+	TpmsRr    string `json:"tpmsRr"`
 }
 
 const dsn string = "root:1234@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 
 func getAbnormalOperationVehicleList(w http.ResponseWriter, r *http.Request) {
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,       // Disable color
-		},
-	)
-
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: newLogger,
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				Colorful:                  true,        // Disable color
+			},
+		),
 	})
 
 	if err != nil {
@@ -95,4 +173,65 @@ func getAbnormalOperationVehicleList(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(vehicles)
 
+}
+
+func GetAvailableVehicleList(w http.ResponseWriter, r *http.Request) {
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				Colorful:                  true,        // Disable color
+			},
+		),
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var model Model
+	db.Table("model").Find(&model)
+
+	var branch Branch
+	db.Table("branch").Find(&branch)
+
+	var business Business
+	db.Table("business").Find(&business)
+
+	var insurance Insurance
+	db.Table("insurance").Find(&insurance)
+
+	var state State
+	db.Table("state").Find(&state)
+
+	var vehicle Vehicle
+	db.Table("vehicle").Find(&vehicle)
+
+	if vehicle.Id == "" {
+		json.NewEncoder(w).Encode(map[string]string{"result": "데이터가 없습니다."})
+		return
+	}
+
+	vehicle.Business = business
+	vehicle.Insurance = insurance
+	vehicle.Model = model
+	vehicle.Branch = branch
+	vehicle.State = state
+
+	var vehicles Vehicles
+	vehicles.Vehicles = []Vehicle{vehicle}
+	vehicles.PageInfo = PageInfo{
+		TotalRecord: 1,
+		TotalPage:   1,
+		Limit:       15,
+		Page:        1,
+		PrevPage:    1,
+		NextPage:    1,
+	}
+
+	json.NewEncoder(w).Encode(vehicles)
 }
